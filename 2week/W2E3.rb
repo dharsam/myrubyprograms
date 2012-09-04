80ac1679beb6e230531343a89df2a7908ed522dd
dharg <dhargaurav@gmail.com>
2012-09-04 01:19:29 -0400

    commit

--- 2week/W2E3.rb
+++ 2week/W2E3.rb
@@ -6,39 +6,17 @@ in a leap year (2000 and 2004) and the number of minutes in a non-leap year
 divisible by 400.
 =end
 
-<<<<<<< HEAD
-
-
-puts "Enter the year: "
-STDOUT.flush
-year = gets.to_i
-
 def leap_year?(year)
   if year % 4 == 0 and year % 100 != 0  || 400 == 0 
     return true
   else
     return false
-=======
-def leap_year?(year)
-  case
-  when year % 4 == 0 then true
-  when year % 100  && 400 == 0 then true
-  when year % 400 == 0 then true
-  else
-    false
->>>>>>> 49b7b3dbbc2dadb13c87228d5344e0b1059d3beb
   end
 end
 
-
-<<<<<<< HEAD
-=======
 puts "Enter the year: "
 STDOUT.flush
 year = gets
-
-
->>>>>>> 49b7b3dbbc2dadb13c87228d5344e0b1059d3beb
 
 leap_year?(year) do |leap_year|
   if leap_year? == true
@@ -46,8 +24,5 @@ leap_year?(year) do |leap_year|
   else
     puts "#{year} is not a leap year."
   end
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> 49b7b3dbbc2dadb13c87228d5344e0b1059d3beb
+
