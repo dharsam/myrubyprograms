e42864b3092ad6a8f72f48bec1c43585afd55594
Victor Goff <keeperotphones@gmail.com>
2012-09-04 00:51:19 -0500

    refactored leap_year method to a conditional statement

--- 2week/W2E3.rb
+++ 2week/W2E3.rb
@@ -6,23 +6,36 @@ in a leap year (2000 and 2004) and the number of minutes in a non-leap year
 divisible by 400.
 =end
 
+=begin
+doctest: leap_year? returns true for leap year centuries
+>> century_years = [1600, 2000, 2400]
+>> results = century_years.collect {|year| leap_year?(year)}
+=> [true, true, true]
+doctest: leap_year? returns false for non-leap years
+>> standard_years = [1901, 1999, 2010]
+>> results = standard_years.collect {|year| leap_year?(year)}
+=> [false, false, false]
+doctest: century years that aren't divisible by 400 are standard
+>> standard_century_years = [1900, 1700, 1800]
+>> results = standard_century_years.collect {|year| leap_year?(year)}
+=> [false, false, false]
+=end
+
 def leap_year?(year)
-  if year % 4 == 0 and year % 100 != 0  || 400 == 0 
-    return true
-  else
-    return false
-  end
+  year % 4 == 0 and year % 100 != 0 or year % 400 == 0
 end
 
-puts "Enter the year: "
-STDOUT.flush
-year = gets
+if __FILE__ == $0
+  puts "Enter the year: "
+  STDOUT.flush
+  # year = gets
 
-leap_year?(year) do |leap_year|
-  if leap_year? == true
-    puts "#{year} is a leap year."
-  else
-    puts "#{year} is not a leap year."
+  leap_year?(year) do |leap_year|
+    if leap_year? == true
+      puts "#{year} is a leap year."
+    else
+      puts "#{year} is not a leap year."
+    end
   end
-end
 
+end
